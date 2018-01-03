66f942bad1e65e7fc4e40563254450257cbfe774
Anthony Kluba <anthonykluba@Anthonys-MacBook-Pro-2.local>
2018-01-02 20:42:07 -0500

    Adjusting setup category response.

--- app/Http/Controllers/Setup/CategoryController.php
+++ app/Http/Controllers/Setup/CategoryController.php
@@ -31,7 +31,7 @@ class CategoryController extends Controller
         ];
 
         $response = [
-            'message' => 'List of all Shop Categories',
+            'message' => "List of all {$source_class} Categories",
             'data'    => $data
         ];
 
