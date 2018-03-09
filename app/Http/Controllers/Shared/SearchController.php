ca7a0ece5fe4c6b81f3fec753c137506f5c53b12
Anthony Kluba <anthonykluba@gmail.com>
2018-03-09 00:01:25 -0500

    Created endpoint to return results of advanced search.

--- app/Http/Controllers/Shared/SearchController.php
+++ app/Http/Controllers/Shared/SearchController.php
@@ -8,76 +8,109 @@ use App\Field;
 
 class SearchController extends Controller
 {
+    /**
+     * [index description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
 	public function index(Request $request)
     {
     	$request->validate([
             'source_class' => 'required'
         ]);
 
-        $source_class = ucfirst($request->input('source_class'));
+        $source_class = $request->input('source_class');
 
     	$fields = Field::where('source_class', $source_class)
-    		->whereIn('type', ['log','notes'])
+    		->whereIn('type', ['log', 'notes'])
     		->get();
 
+        $grouped = $fields->mapToGroups(function ($field, $key) use ($source_class) {
+            $field_columns = $field->columns()->get();
+
+            if ($field->type === 'log') {
+                foreach ($field_columns as $column) {
+                    if (in_array($column->type, ['select','select_multiple']))
+                        $column->options;
+                }
+            } elseif ($field->type === 'notes') {
+                $field_columns = $field_columns->where('system', null);
+
+                foreach ($field_columns as $key => $column) {
+                    if (in_array($column->type, ['manager_link','shop_link'])) {
+                        $link_source_class = str_replace('_link', '', $column->type);
+                        $field_columns[$key]['options'] = $this->getSourceRecords($link_source_class);
+                    }
+                }
+
+                $created_for = (object) [
+                    'title' => 'Created For',
+                    'column_name' => 'source_id',
+                    'type' => $this->noteType($source_class),
+                    'options' => $this->getSourceRecords($source_class)
+                ];
+
+                if ($created_for->type === 'select') {
+                    $created_for->options->map(function ($option) {
+                        return $option->title = $option->name;
+                    });
+                }
+
+                $field_columns[] = $created_for;
+                $field_columns = $field_columns->flatten(1);
+            }
+
+            $field->columns = $field_columns;
+
+            return [$field['type'] => $field];
+        });
+
+        $grouped = $grouped->toArray();
+
     	$response = [
-    		'fields' => $fields->toArray()
+    		'fields' => $grouped
     	];
 
     	return response()->json($response, 200);
     }
 
+    /**
+     * [results description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
     public function results(Request $request)
     {
-
-    	$request->validate([
+        $request->validate([
             'source_class' => 'required',
-            'filters' => 'required',
+            'filters' => 'required'
         ]);
 
         $source_class = $request->input('source_class');
-    	$filters = $request->input('filters');
+        $filters = $request->input('filters');
 
-    	// Get all source_class log entries.
-    	$source_log_entries = \App\LogEntry::where('source_class', $source_class)
-    	 	->get();
+        $query = \App\LogEntry::select();
 
-    	// Group log entries by source_id.
-    	$grouped_log_entries = $source_log_entries
-    		->groupBy('source_id');
+        foreach ($filters as $field_id => $field_filters) {
+            // Filter on field_id.
+            $query->where('field_id', $field_id);
 
-    	// Loop through grouped log entry arrays, check for matches.
-    	$matches = $grouped_log_entries->map(function ($source) use ($filters) {
-    		dd($source->toArray());
-    	});
+            // Filter out null filters.
+            $set_filters = array_filter($field_filters, function($filter_array) {
+                return array_filter($filter_array, function($filter) {
+                    return $filter;
+                });
+            });
 
-    	$query = \App\LogEntry::select();
+            if ($set_filters) {
+                foreach ($set_filters as $filter_group) {
+                    $this->setFilters($query, $filter_group);
+                }
+            }
+        }
 
-    	// Build query where methods.
-    	foreach ($filters as $field => $field_filters) {
-    		$query->where('field_id', $field);
-    		$query->where(function ($query) use ($field_filters) {
-    			$i = 0;
-				foreach ($field_filters as $filter_group) {
-					// Wrap the first field filter group in where function.
-					if ($i == 0)
-						$query->where(function ($query) use ($filter_group) {
-							$this->setFilters($query, $filter_group);
-						});
-
-					// Wrap additional field filters in orWhere function.
-					else
-						$query->orWhere(function ($query) use ($filter_group) {
-							$this->setFilters($query, $filter_group);
-						});
-
-					$i++;
-				}
-			});
-		}
-
-		// Get all the log entry records that match the filters.
-		$matches = $query->get();
+        // Get all the log entry records that match the filters.
+        $matches = $query->get();
 
 		// Create array of ids that matched filters.
 		$sources = $matches
@@ -89,13 +122,34 @@ class SearchController extends Controller
 
 		// Filter records to those that match the filter criteria.
 		$filtered_records = $records
-			->whereIn('id', $sources);
+			->whereIn('id', $sources)
+            ->flatten(1);
+
+        $field_array = \App\Category::elements($source_class);
+
+        $filtered_records->map(function ($record) use ($field_array){
+            foreach ($field_array as $custom => $field) {
+                if (!is_null($record[$custom]) && in_array($field['type'], array('select','select_multiple'))) {
+                    if ($field['type'] === 'select_multiple') {
+                        $option_array = array();
+                        foreach (json_decode($record[$custom]) as $option) {
+                            $option_array[] = $field['options'][$option]['title'];
+                        }
+                        $record[$custom] = $option_array;
+                    } else {
+                        $record[$custom] = $field['options'][$record[$custom]]['title'];
+                    }
+                } elseif ($field['type'] === 'checkbox') {
+                    $record[$custom] = $record[$custom] ? 'true' : 'false';
+                }
+            }
+
+            return $record;
+        });
 
     	$response = [
-    		'filters' => $filters,
-    		'matches' => $matches,
-    		'sources' => $sources,
-    		'records' => $filtered_records
+            'matches' => $filtered_records,
+            'fields' => $field_array
     	];
 
     	return response()->json($response, 200);
@@ -104,10 +158,22 @@ class SearchController extends Controller
     private function setFilters($query, $filters)
     {
     	foreach ($filters as $col => $value) {
-			$query->where($col, $value);
+			if ($value)
+                $query->where($col, $value);
 		}
 
     	return $query;
+    }
+
+    private function noteType($source_class)
+    {
+        if ($source_class === 'shop') {
+            return 'shop_link';
+        } elseif ($source_class === 'manager') {
+            return 'manager_link';
+        } else {
+            return 'select';
+        }
     }
 
     private function getSourceRecords($source_class)
@@ -122,6 +188,9 @@ class SearchController extends Controller
     		case 'vendor':
     			return \App\Vendor::all();
     			break;
+            case 'cpr':
+                return \App\CPR::all();
+                break;
     	}
     }
 
