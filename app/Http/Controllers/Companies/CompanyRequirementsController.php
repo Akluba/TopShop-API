84855b96741b91fa88a6788e248d9f625a711dbd
Anthony Kluba <anthonykluba@gmail.com>
2018-03-23 18:51:16 -0400

    Requirement list added to index endpoint, also created destroy endpoint.

--- app/Http/Controllers/Companies/CompanyRequirementsController.php
+++ app/Http/Controllers/Companies/CompanyRequirementsController.php
@@ -16,11 +16,21 @@ class CompanyRequirementsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $request->validate([
+            'company_id' => 'required'
+        ]);
+
+        $company_id = $request->input('company_id');
+
+        $company = Company::find($company_id);
+
+        $company_requirements = $company->requirements;
+
         // Get Shop fields.
         $fields = Field::where('source_class', 'Shop')
-                    ->whereNotIn('type', ['notes', 'log'])
+                    ->whereIn('type', ['text', 'checkbox','select','select_multiple'])
                     ->get();
 
         // Get Select Options.
@@ -30,29 +40,69 @@ class CompanyRequirementsController extends Controller
             }
         });
 
+        $data = [
+            'fields'       => $fields,
+            'requirements' => $company_requirements
+        ];
+
         $response = [
             'message' => 'List of all Companies',
-            'data'    => $fields
+            'data'    => $data
         ];
 
         return response()->json($response, 200);
     }
 
     /**
-     * Display the specified resource.
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'company_id' => 'required',
+            'field_id'   => 'required',
+            'condition'  => 'required',
+            'value'      => 'required'
+        ]);
+
+        $company_id = $request->input('company_id');
+        $field_id = $request->input('field_id');
+        $condition = $request->input('condition');
+        $value = $request->input('value');
+
+        $company_requirement = new CompanyRequirement;
+        $company_requirement->company_id = $company_id;
+        $company_requirement->field_id = $field_id;
+        $company_requirement->condition = $condition;
+        $company_requirement->value = $value;
+        $company_requirement->save();
+
+        $response = [
+            'message' => "Company Requirement: has been added.",
+            'data'    => $company_requirement
+        ];
+
+        return response()->json($response, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function destroy($id)
     {
-        $company = Company::find($id);
+        $requirement = CompanyRequirement::find($id);
 
-        $company_requirements = $company->requirements;
+        $requirement->delete();
 
         $response = [
-            'message' => 'List of Company Requirements',
-            'data'    => $company_requirements
+            'message' => "Requirement has been deleted.",
+            'data'    => $requirement
         ];
 
         return response()->json($response, 200);
